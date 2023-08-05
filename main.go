4920082b079da15059b6abc84a5893ee14aa2ae2
adityamahendrap <ptadityamahendrap@gmail.com>
2023-08-05 19:04:59 +0800

    add env

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
     "fmt"
     "email-sender/helper"
+    "log"
+    "net/smtp"
+    "strings"
 )
 
 func main() {
     fmt.Println("Hello, world.")
     helper.Help()
+    
 }
