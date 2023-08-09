cf483d144e6aebf1094723284523fd052c12ff26
adityamahendrap <ptadityamahendrap@gmail.com>
2023-08-09 22:59:53 +0800

    divide to function

--- helper/helper.go
+++ helper/helper.go
@@ -1,7 +1,47 @@
 package helper
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"path/filepath"
+	// "log"
+	// "path/filepath"
+)
 
-func Help() {
-  fmt.Println("This is a helper function")
+type BodyJson struct {
+    Subject string `json:"subject"`
+    Message string `json:"message"`
 }
+
+func GenerateAbsolutePath(relativePath string) string {
+    currentPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
+    if err != nil {
+        log.Fatal(err)
+    }
+    
+    bodyPath := filepath.Join(currentPath, relativePath)
+    return bodyPath
+}
+
+func ReadFile(path string) ([]byte, error) {
+    file, err := os.Open(path)
+    if err!= nil {
+        return nil, err
+    }
+    defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+        return nil, err
+	}
+	fileSize := fileInfo.Size()
+
+	fileContent := make([]byte, fileSize)
+	_, err = file.Read(fileContent)
+	if err != nil {
+        return nil, err
+	}
+    
+    return fileContent, nil
+}
+
--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"email-sender/helper"
+	"encoding/json"
 	"fmt"
 	"log"
-	"os"
-	"strings"
 	"net/smtp"
+	"os"
+	// "path/filepath"
 	"strconv"
+	"strings"
+
 	"github.com/joho/godotenv"
-	// "log"
-	// "strings"
 )
 
 type Config struct {
@@ -20,6 +22,19 @@ type Config struct {
 	Port int
 }
 
+type Attachment struct {
+	Filename string
+	Data     []byte
+	Inline   bool
+}
+
+type Message struct {
+    To[] string
+    Subject string
+    Message string
+    Attachments map[string]*Attachment
+}
+
 func getEnv(key string) string {
     err := godotenv.Load()
     if err != nil {
@@ -28,17 +43,33 @@ func getEnv(key string) string {
     return os.Getenv(key)
 }
 
-func sendMail(conf Config, to []string, cc []string, subject, message string) error {
+func (c *Config) Load() *Config {
+    c.Email = getEnv("CONFIG_AUTH_EMAIL")
+	c.Name = getEnv("CONFIG_SENDER_NAME")
+	c.Password = getEnv("CONFIG_AUTH_PASSWORD")
+	c.Host = getEnv("CONFIG_SMTP_HOST")
+	c.Port, _ = strconv.Atoi(getEnv("CONFIG_SMTP_PORT"))
+    return c
+}
+
+func (m *Message) New(to []string, subject string, message string, attachments map[string]*Attachment) *Message {
+    m.To = to
+    m.Subject = subject
+    m.Message = message
+    m.Attachments = attachments
+    return m
+}
+
+func Send(conf Config, email Message) error {
 	body := "From: " + conf.Name + "\n" +
-		    "To: " + strings.Join(to, ",") + "\n" +
-		    "Cc: " + strings.Join(cc, ",") + "\n" +
-		    "Subject: " + subject + "\n\n" +
-		    message
+		    "To: " + strings.Join(email.To, ",") + "\n" +
+		    "Subject: " + email.Subject + "\n\n" +
+		    email.Message
 
 	auth := smtp.PlainAuth("", conf.Email, conf.Password, conf.Host)
 	smtpAddr := fmt.Sprintf("%s:%d", conf.Host, conf.Port)
 
-	err := smtp.SendMail(smtpAddr, auth, conf.Email, append(to, cc...), []byte(body))
+	err := smtp.SendMail(smtpAddr, auth, conf.Email, email.To, []byte(body))
 	if err != nil {
 		return err
 	}
@@ -46,21 +77,40 @@ func sendMail(conf Config, to []string, cc []string, subject, message string) er
 	return nil
 }
 
+
+func loadFromJsonFile(relativeFilePath string) helper.BodyJson {
+    absoluteFilePath := helper.GenerateAbsolutePath(relativeFilePath)
+    
+    fileContentJson, err := helper.ReadFile(absoluteFilePath)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Print(string(fileContentJson))
+
+    var body helper.BodyJson
+    err = json.Unmarshal(fileContentJson, &body)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return body
+}
+
 func main() {
-	var conf Config
+    relativeFilePath := "resource/body.json"
+    j := loadFromJsonFile(relativeFilePath)
 
-	conf.Email = getEnv("CONFIG_AUTH_EMAIL")
-	conf.Name = getEnv("CONFIG_SENDER_NAME")
-	conf.Password = getEnv("CONFIG_AUTH_PASSWORD")
-	conf.Host = getEnv("CONFIG_SMTP_HOST")
-	conf.Port, _ = strconv.Atoi(getEnv("CONFIG_SMTP_PORT"))
+    message := strings.ReplaceAll(strings.ReplaceAll(j.Message, "[nama]", "Test"), "[perusahaan]", "PT Djarum")
+    to := []string{"ptadityamahendrap@gmail.com", "whatupbiatch69@gmail.com"}
+    subject := j.Subject
+    
+    var m Message
+    var conf Config 
+    
+    conf.Load()
+    m.New(to, subject, message, nil)
 
-	to := []string{"ptadityamahendrap@gmail.com"}
-	cc := []string{"whatupbiatch69@gmail.com"}
-	subject := "Test mail"
-	message := "Hello"
-
-	err := sendMail(conf, to, cc, subject, message)
+	err := Send(conf, m)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
